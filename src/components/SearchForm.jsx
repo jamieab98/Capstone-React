636e9dd9db70d757b64c1f08ff8802aa7865925e
Jamie Bryson <jamieab98@gmail.com>
2026-05-04 19:53:45 -0400

    Changed one of the states to numberOfReturns and started making the fields for the search

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -3,11 +3,24 @@ import { useState } from "react"
 function SearchForm(){
 
     const [searchParameter, setSearchParameter] = useState("")
-    const [numberPerPage, setNumberPerPage] = useState(1)
+    const [numberOfReturns, setNumberOfReturns] = useState(1)
 
     return(
         <>
             <h2>Search Form Component</h2>
+            <form>
+                <label>Search Parameter</label>
+                <select>
+                    <option>--Select One--</option>
+                    <option>By City</option>
+                    <option>By State</option>
+                    <option>By Name</option>
+                    <option>Random</option>
+                </select>
+                <label>Number of Bars
+                    <input type="number"/>
+                </label>
+            </form>
         </>
     )
 }
