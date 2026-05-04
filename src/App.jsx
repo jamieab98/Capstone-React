a651714358046ea75faa0ba4782f61f9b1b5a3c6
Jamie Bryson <jamieab98@gmail.com>
2026-05-04 19:41:02 -0400

    took the url and saved it as a constant in App.jsx. Also created the state that will be used in SearchForm.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import SearchForm from "./components/SearchForm"
 
 function App(){
 
+  const url = "https://api.openbrewerydb.org/v1/breweries"
+  //search by_city example: https://api.openbrewerydb.org/v1/breweries?by_city=san_diego&per_page=3
+
   return(
     <>
       <h1>App Components</h1>
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react"
+
 function SearchForm(){
+
+    const [searchParameter, setSearchParameter] = useState("")
+    const [numberPerPage, setNumberPerPage] = useState(1)
 
     return(
         <>
