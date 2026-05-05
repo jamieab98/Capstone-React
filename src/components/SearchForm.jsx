6c0ed093ac5c8aa0437786de11ae3e6d4c2a0b05
Jamie Bryson <jamieab98@gmail.com>
2026-05-04 20:07:16 -0400

    added values and onChange to the search inputs. Also created the handleSearch function

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -5,21 +5,27 @@ function SearchForm(){
     const [searchParameter, setSearchParameter] = useState("")
     const [numberOfReturns, setNumberOfReturns] = useState(1)
 
+    function handleSearch(e){
+        e.preventDefault()
+        console.log("Searching for bars")
+    }
+
     return(
         <>
             <h2>Search Form Component</h2>
             <form>
-                <label>Search Parameter</label>
-                <select>
-                    <option>--Select One--</option>
-                    <option>By City</option>
-                    <option>By State</option>
-                    <option>By Name</option>
-                    <option>Random</option>
+                <label htmlFor="searchparameter">Search Parameter</label>
+                <select id="searchparameter" value={searchParameter} onChange={(e)=>setSearchParameter(e.target.value)}>
+                    <option value="">--Select One--</option>
+                    <option value="city">By City</option>
+                    <option value="state">By State</option>
+                    <option value="name">By Name</option>
+                    <option value="random">Random</option>
                 </select>
-                <label>Number of Bars
-                    <input type="number"/>
+                <label id="numberofbars">Number of Bars
+                    <input type="number" id="numberofbars" value={numberOfReturns} onChange={(e)=>setNumberOfReturns(e.target.value)}/>
                 </label>
+                <button type="submit">Search</button>
             </form>
         </>
     )
