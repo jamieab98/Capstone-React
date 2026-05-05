c680c8929420a55bc96b70a425601c33eb398c3e
Jamie Bryson <jamieab98@gmail.com>
2026-05-04 20:27:52 -0400

    Added another field for the user input for the search

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -4,16 +4,18 @@ function SearchForm(){
 
     const [searchParameter, setSearchParameter] = useState("")
     const [numberOfReturns, setNumberOfReturns] = useState(1)
+    const [searchInput, setSearchInput] = useState("")
 
     function handleSearch(e){
         e.preventDefault()
-        console.log("Searching for bars")
+        console.log("Searching for bars...")
+        console.log(`Searching for ${numberOfReturns} bars by ${searchParameter}.`)
     }
 
     return(
         <>
             <h2>Search Form Component</h2>
-            <form>
+            <form onSubmit={handleSearch}>
                 <label htmlFor="searchparameter">Search Parameter</label>
                 <select id="searchparameter" value={searchParameter} onChange={(e)=>setSearchParameter(e.target.value)}>
                     <option value="">--Select One--</option>
@@ -24,6 +26,9 @@ function SearchForm(){
                 </select>
                 <label id="numberofbars">Number of Bars
                     <input type="number" id="numberofbars" value={numberOfReturns} onChange={(e)=>setNumberOfReturns(e.target.value)}/>
+                </label>
+                <label id="userinput">Search By: 
+                    <input type="text" id="userinput" value={searchInput} onChange={(e)=>setSearchInput(e.target.value)}/>
                 </label>
                 <button type="submit">Search</button>
             </form>
